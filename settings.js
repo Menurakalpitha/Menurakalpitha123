35a23037d9b046b437e830d437e9e35b366d8765
Menurakalpitha <116156235+Menurakalpitha@users.noreply.github.com>
2023-04-30 18:04:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
 global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
+global.ownername = "😎𝗚.𝗔.𝗠𝗲𝗻𝘂𝗿𝗮 𝗞𝗮𝗹𝗽𝗶𝘁𝗵𝗮❯❯" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
